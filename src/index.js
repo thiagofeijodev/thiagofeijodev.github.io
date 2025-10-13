883e9a2107f0195161ee98c39dd9d72bc917d034
thiagofeijor <thiago@feijo.dev>
2025-10-13 22:29:36 +0100

    feat: include google tag

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import createGoogleTag from './utils/createGoogleTag';
 import App from './App'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
 
-if ("serviceWorker" in navigator) 
-  navigator.serviceWorker.register("service-worker.js")
+if ("serviceWorker" in navigator)
+navigator.serviceWorker.register("service-worker.js")
+
+createGoogleTag();
