93b77517b03b600b0bd0ace40cba603d69c54a3f
thiagofeijor <thiago@feijo.dev>
2025-10-19 01:44:27 +0100

    fix: change to docs

--- .config/rspack/rspack.prod.mjs
+++ .config/rspack/rspack.prod.mjs
@@ -1,45 +1,45 @@
-import path from 'path';
-import { WebpackAssetsManifest } from 'webpack-assets-manifest';
-import WorkboxPlugin from 'workbox-webpack-plugin';
-import FaviconsRspackPlugin from 'favicons-rspack-plugin';
-import CompressionPlugin from 'compression-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import common from './rspack.common.mjs';
-import rspack from '@rspack/core';
+import path from "path";
+import { WebpackAssetsManifest } from "webpack-assets-manifest";
+import WorkboxPlugin from "workbox-webpack-plugin";
+import FaviconsRspackPlugin from "favicons-rspack-plugin";
+import CompressionPlugin from "compression-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import common from "./rspack.common.mjs";
+import rspack from "@rspack/core";
 
 export default () => {
   const config = {
     ...common,
-    mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+    mode: process.env.NODE_ENV === "development" ? "development" : "production",
     devtool: false,
     output: {
       ...common.output,
-      path: path.resolve(process.cwd(), 'dist'),
+      path: path.resolve(process.cwd(), "docs"),
     },
     plugins: [
-      new rspack.EnvironmentPlugin(['REACT_APP_GA_ID']),
+      new rspack.EnvironmentPlugin(["REACT_APP_GA_ID"]),
       new HtmlWebpackPlugin({
-        favicon: 'public/logo.png',
-        template: path.join(process.cwd(), 'public/index.html'),
-        filename: path.join(process.cwd(), 'dist/index.html'),
-        inject: 'body',
+        favicon: "public/logo.png",
+        template: path.join(process.cwd(), "public/index.html"),
+        filename: path.join(process.cwd(), "docs/index.html"),
+        inject: "body",
       }),
       new FaviconsRspackPlugin({
-        logo: './public/logo.png',
+        logo: "./public/logo.png",
         cache: true,
         inject: true,
-        mode: 'webapp',
-        manifest: './public/manifest.json',
+        mode: "webapp",
+        manifest: "./public/manifest.json",
       }),
       new CompressionPlugin({
-        filename: '[path][base].gz',
+        filename: "[path][base].gz",
       }),
       new WebpackAssetsManifest({}),
       new WorkboxPlugin.GenerateSW(),
     ],
     optimization: {
       splitChunks: {
-        chunks: 'all',
+        chunks: "all",
       },
     },
   };
