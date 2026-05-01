7e2c073194328d33156b19485f9e1ad77b92a07a
thiagofeijor <thiago@feijo.dev>
2026-05-01 08:58:09 +0100

    feat: snake svg

--- .config/tests/setupTests.js
+++ .config/tests/setupTests.js
@@ -2,3 +2,15 @@
 const { TextEncoder, TextDecoder } = require("util");
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+
+global.IntersectionObserver = class {
+  constructor() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+if (typeof SVGPathElement !== "undefined") {
+  SVGPathElement.prototype.getTotalLength = () => 0;
+  SVGPathElement.prototype.getPointAtLength = () => ({ x: 0, y: 0 });
+}
